c85bc0099be2465b8ba212494f8300d71189da2f
Angelo Aldair Giron Corro <angelo.giron@orbis.com.pe>
2021-08-14 10:21:16 -0500

    Fix(Layout): Wordpress API Url modified

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ import Meta from '../meta'
 import { GlobalStyles } from './styled'
 import { request } from 'graphql-request'
 
-const handleFetch = query => request('https://animesama.net/graphql', query)
+const handleFetch = query => request(process.env.WORDPRESS_API_URL, query)
 
 export default function Layout({ children }) {
     const { data, error } = useSWR(`{
